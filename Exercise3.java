7092d749162936922b5b2deae1fa829a73c9c57e
mouh <mouh>
2026-04-29 19:51:15 +0100

    2

--- Exercise3.java
+++ Exercise3.java
@@ -15,40 +15,39 @@ public class Exercise3 {
 
         Graph<String, DefaultEdge> cityGraph = new SimpleGraph<>(DefaultEdge.class);
 
-        cityGraph.addVertex("Algiers");
-        cityGraph.addVertex("costantine");
-        // personal fav
-        cityGraph.addVertex("Jijel");
-        cityGraph.addVertex("setif");
-        cityGraph.addVertex("media");
+        cityGraph.addVertex("Alger");
+        cityGraph.addVertex("Blida");
+        cityGraph.addVertex("Tipaza");
+        cityGraph.addVertex("Boumerdes");
+        cityGraph.addVertex("Medea");
 
-        cityGraph.addVertex("costantine");
-        cityGraph.addVertex("Jijel");
-        cityGraph.addVertex("setif");
-        cityGraph.addVertex("media");
+        cityGraph.addEdge("Alger", "Blida");
+        cityGraph.addEdge("Alger", "Tipaza");
+        cityGraph.addEdge("Alger", "Boumerdes");
+        cityGraph.addEdge("Blida", "Medea");
 
         System.out.println("Graph vertices: " + cityGraph.vertexSet());
         System.out.println("Graph edges: " + cityGraph.edgeSet());
 
-        System.out.println("BFS traversal from Algiers:");
-        BreadthFirstIterator<String, DefaultEdge> searchOne = new BreadthFirstIterator<>(cityGraph, "Algiers");
+        System.out.println("BFS traversal from Alger:");
+        BreadthFirstIterator<String, DefaultEdge> searchOne = new BreadthFirstIterator<>(cityGraph, "Alger");
         while (searchOne.hasNext()) {
             System.out.print(searchOne.next() + " ");
         }
         System.out.println();
 
-        cityGraph.addVertex("Tamanrasset");
+        cityGraph.addVertex("Oran");
 
-        System.out.println("BFS traversal after adding Tamanrasset (unconnected node):");
-        BreadthFirstIterator<String, DefaultEdge> searchTwo = new BreadthFirstIterator<>(cityGraph, "Algiers");
+        System.out.println("BFS traversal after adding Oran (unconnected node):");
+        BreadthFirstIterator<String, DefaultEdge> searchTwo = new BreadthFirstIterator<>(cityGraph, "Alger");
         while (searchTwo.hasNext()) {
             System.out.print(searchTwo.next() + " ");
         }
         System.out.println();
 
-        cityGraph.addEdge("Jijel", "media");
-        System.out.println("BFS traversal after connecting Jijel and media:");
-        BreadthFirstIterator<String, DefaultEdge> searchThree = new BreadthFirstIterator<>(cityGraph, "Algiers");
+        cityGraph.addEdge("Tipaza", "Medea");
+        System.out.println("BFS traversal after connecting Tipaza and Medea:");
+        BreadthFirstIterator<String, DefaultEdge> searchThree = new BreadthFirstIterator<>(cityGraph, "Alger");
         while (searchThree.hasNext()) {
             System.out.print(searchThree.next() + " ");
         }
@@ -56,38 +55,38 @@ public class Exercise3 {
 
         Graph<String, DefaultWeightedEdge> weightedCityGraph = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
 
-        String[] mappedCities = { "Algiers", "costantine", "Jijel", "setif", "media" };
+        String[] mappedCities = { "Alger", "Blida", "Tipaza", "Boumerdes", "Medea" };
         for (String city : mappedCities) {
             weightedCityGraph.addVertex(city);
         }
 
-        DefaultWeightedEdge edgeA = weightedCityGraph.addEdge("Algiers", "costantine");
-        weightedCityGraph.setEdgeWeight(edgeA, 20.0);
+        DefaultWeightedEdge edgeA = weightedCityGraph.addEdge("Alger", "Blida");
+        weightedCityGraph.setEdgeWeight(edgeA, 50.0);
 
-        DefaultWeightedEdge edgeB = weightedCityGraph.addEdge("Algiers", "costantine");
-        weightedCityGraph.setEdgeWeight(edgeB, 50.0);
+        DefaultWeightedEdge edgeB = weightedCityGraph.addEdge("Alger", "Tipaza");
+        weightedCityGraph.setEdgeWeight(edgeB, 70.0);
 
-        DefaultWeightedEdge edgeC = weightedCityGraph.addEdge("Algiers", "setif");
+        DefaultWeightedEdge edgeC = weightedCityGraph.addEdge("Alger", "Boumerdes");
         weightedCityGraph.setEdgeWeight(edgeC, 45.0);
 
-        DefaultWeightedEdge edgeD = weightedCityGraph.addEdge("costantine", "media");
+        DefaultWeightedEdge edgeD = weightedCityGraph.addEdge("Blida", "Medea");
         weightedCityGraph.setEdgeWeight(edgeD, 90.0);
 
-        DefaultWeightedEdge edgeE = weightedCityGraph.addEdge("costantine", "Jijel");
+        DefaultWeightedEdge edgeE = weightedCityGraph.addEdge("Boumerdes", "Tipaza");
         weightedCityGraph.setEdgeWeight(edgeE, 80.0);
 
-        DefaultWeightedEdge edgeF = weightedCityGraph.addEdge("Jijel", "media");
+        DefaultWeightedEdge edgeF = weightedCityGraph.addEdge("Tipaza", "Medea");
         weightedCityGraph.setEdgeWeight(edgeF, 60.0);
 
         DijkstraShortestPath<String, DefaultWeightedEdge> pathSolver = new DijkstraShortestPath<>(weightedCityGraph);
 
-        System.out.println("\nShortest path between Algiers and Jijel:");
-        GraphPath<String, DefaultWeightedEdge> firstPath = pathSolver.getPath("Algiers", "Jijel");
+        System.out.println("\nShortest path between Alger and Medea:");
+        GraphPath<String, DefaultWeightedEdge> firstPath = pathSolver.getPath("Alger", "Medea");
         System.out.println("Path route: " + firstPath.getVertexList());
         System.out.println("Total distance: " + firstPath.getWeight());
 
-        System.out.println("\nShortest path between costantine and Jiejl:");
-        GraphPath<String, DefaultWeightedEdge> secondPath = pathSolver.getPath("costantine", "Jijel");
+        System.out.println("\nShortest path between Boumerdes and Blida:");
+        GraphPath<String, DefaultWeightedEdge> secondPath = pathSolver.getPath("Boumerdes", "Blida");
         System.out.println("Path route: " + secondPath.getVertexList());
         System.out.println("Total distance: " + secondPath.getWeight());
     }
